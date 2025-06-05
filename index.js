d6cc1e328bf062e99614291ee6b027bf556d18aa
Alec Strand <alexander@strand.it>
2025-06-05 10:53:19 +0200

    added a logger function (will move later)

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express')
-const notesRouter = require('./notesRouter')
+const notesRouter = require('./routes/notesRouter')
 const app = express()
 
 app.use(express.json())
 
 const PORT = 2999
+
+
+// Logging middleware, will change position later when I figure that out.
+
+app.use(logger = (req, res, next) => {
+    req.time = new Date(Date.now()).toString();
+    console.log(req.method, req.hostname, req.path, req.time)
+    next()
+})
 
 // Here we mount the Notes Router. 
 app.use('/notes', notesRouter)
